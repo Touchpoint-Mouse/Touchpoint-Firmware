e3a27c342f385200527ea3ed8c195ddebdf80e3e
Carson Ray <carsonray314@gmail.com>
2026-02-21 12:44:51 -0500

    LRA haptic driver working

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,7 @@
 #include <Adafruit_NeoPixel.h>
 //#include <SongbirdCore.h>
 //#include <SongbirdUART.h>
-#include "../integration/imu_test.hpp" // Testing file to run
+#include "../integration/haptic_driver_test.hpp" // Testing file to run
 #endif
 //////////////////////////////////////////////////////////////
 
